1a4342cc5f4d808d40bd1fc3ce74e2ed99def5fe
Kumonda221 <kumonda@foxmail.com>
2015-07-12 23:49:38 +0800

    Update ArrayVector.java

--- src/main/java/com/mengcraft/playersql/util/ArrayVector.java
+++ src/main/java/com/mengcraft/playersql/util/ArrayVector.java
@@ -8,55 +8,63 @@ public class ArrayVector<E> implements Iterator<E> {
     private int cursor;
 
     @Override
-    public boolean hasNext() {
+    public boolean hasNext() 
+    {
         return array.length != cursor;
     }
 
     @Override
-    public E next() {
+    public E next() 
+    {
         return hasNext() ? array[cursor++] : null;
     }
 
     @Override
-    public void remove() {
+    public void remove() 
+    {
         throw new UnsupportedOperationException();
     }
     
-    public boolean contains(E element) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(element)) { return true; }
-        }
+    public boolean contains(E element)
+    {
+        for (int i = 0; i < array.length; i++) 
+            if (array[i].equals(element))
+                return true;
         return false;
     }
 
-    public E get(int index) {
-        if (index < 0 || index >= array.length) {
+    public E get(int index) 
+    {
+        if (index < 0 || index >= array.length)
             throw new IndexOutOfBoundsException();
-        }
         return array[index];
     }
 
-    public E get() {
+    public E get() 
+    {
         return hasNext() ? array[cursor] : null;
     }
 
-    public int remain() {
+    public int remain() 
+    {
         return array.length - cursor;
     }
 
-    public int cursor() {
+    public int cursor() 
+    {
         return cursor;
     }
 
-    public E[] array() {
+    public E[] array() 
+    {
         return array;
     }
 
     @SuppressWarnings("unchecked")
-    public ArrayVector(E... array) {
-        if (array == null) {
+    public ArrayVector(E... array) 
+    {
+        if (array == null)
             throw new NullPointerException();
-        }
         this.array = array;
     }
 
